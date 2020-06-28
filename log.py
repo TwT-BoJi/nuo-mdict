9ac24c4e176435610e3535893a1cb226ac897327
tao_home <tao@doocom.cn>
2020-06-28 13:44:12 +0800

    use python

---
+++ log.py
@@ -0,0 +1,11 @@
+import sys
+from datetime import datetime
+
+
+def log(x):
+    v = sys.version_info
+    version = f'{v.major}.{v.minor}.{v.micro}'
+    time = f'{datetime.now():%Y/%m/%d %H:%M:%S.%f}'
+    print(f'>>>> {time} | python: {version}')
+    print(f'{x}')
+    print(f'<<<<')
--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-from datetime import datetime
+from log import log
+from zlib import adler32
 
 
-def log(x):
-    time = datetime.now()
-    print(f'#### {time:%Y-%m-%d %H:%M:%S.%f}')
-    print(f'>>>> {x}')
-    print()
+class Range:
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+
+
+class Section:
+    def __init__(self, binary, offset, length):
+        self.binary = binary
+        self.offset = offset
+        self.length = length
+
+    @property
+    def range(self):
+        return Range(self.offset, self.offset + self.length)
+
+    @property
+    def bytes(self):
+        s = slice(self.range.min, self.range.max)
+        b = self.binary[s]
+        return b
+
+
+class DictInfoLength(Section):
+    @property
+    def dict_info_length(self):
+        return int.from_bytes(self.bytes, 'big')
+
+
+class DictInfo(Section):
+    @property
+    def xml(self):
+        return self.bytes.decode('utf-8')
+
+    @property
+    def checksum(self):
+        return adler32(self.bytes)
+
+
+class DictInfoChecksum(Section):
+    @property
+    def checksum(self):
+        return int.from_bytes(self.bytes, 'little')
 
 
 def main():
     with open('./mdict/coca.mdx', 'rb') as file:
-        log(file)
+        binary = file.read()
+
+        dl = DictInfoLength(binary, 0, 4)
+        di = DictInfo(binary, dl.range.max, dl.dict_info_length)
+        dc = DictInfoChecksum(binary, di.range.max, 4)
+
+        log(f'di checksum {di.checksum}')
+        log(f'hc checksum {dc.checksum}')
 
 
-main()
+if __name__ == '__main__':
+    main()
