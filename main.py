974fde5a4908fe5b0181be8977c569c04300f156
tao_home <tao@doocom.cn>
2020-07-12 23:21:30 +0800

    .

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from zlib import adler32
-from xml.dom.minidom import parseString as parse_xml
+import zlib
+from struct import pack, unpack
 
+from ripemd128 import ripemd128
 from log import log
 
 
@@ -9,11 +10,11 @@ def test(exp, msg = ''):
         log(f'!!!! test fail {msg}')
 
 
-def int_from_byte_be(b):
+def uint_from_byte_be(b):
     return int.from_bytes(b, 'big')
 
 
-def int_from_byte_le(b):
+def uint_from_byte_le(b):
     return int.from_bytes(b, 'little')
 
 
@@ -21,17 +22,35 @@ def part(sequence, offset, length):
     return sequence[slice(offset, offset + length)]
 
 
+def _fast_decrypt(data, key):
+    b = bytearray(data)
+    key = bytearray(key)
+    previous = 0x36
+    for i in range(len(b)):
+        t = (b[i] >> 4 | b[i] << 4) & 0xff
+        t = t ^ previous ^ (i & 0xff) ^ key[i % len(key)]
+        previous = b[i]
+        b[i] = t
+    return bytes(b)
+
+
+# TODO: rename
+def _mdx_decrypt(comp_block):
+    key = ripemd128(comp_block[4:8] + pack(b'<L', 0x3695))
+    return comp_block[0:8] + _fast_decrypt(comp_block[8:], key)
+
+
 def analyze_section_header(binary, offset):
     t = part(binary, offset + 0, 4)
-    len_xml = int_from_byte_be(t)
+    len_xml = uint_from_byte_be(t)
 
     xml_b = part(binary, offset + 4, len_xml)
     xml_s = xml_b.decode('utf-16le')
 
     t = part(binary, offset + 4 + len_xml, 4)
-    checksum = int_from_byte_le(t)
+    checksum = uint_from_byte_le(t)
 
-    assert adler32(xml_b) == checksum
+    assert zlib.adler32(xml_b) == checksum
 
     size = 4 + len_xml + 4
     data = {
@@ -53,16 +72,63 @@ def analyze_section_keyword(binary, offset):
     data = {}
     for name, index, length in array:
         b = part(binary, offset + index, length)
-        i = int_from_byte_be(b)
+        i = uint_from_byte_be(b)
         data[name] = i
 
     b = part(binary, offset + 40, 4)
-    checksum = int_from_byte_be(b)
+    checksum = uint_from_byte_be(b)
 
     p = part(binary, offset + 0, 40)
-    assert adler32(p) == checksum
+    assert zlib.adler32(p) == checksum
+
+    analyze_keyword_index(binary, offset + 44, data)
 
     return data
+
+
+# TODO: rename `keyword_block_info` `keyword_block_mate` `block_mate`
+def analyze_keyword_index(binary, offset, meta):
+    p = part(binary, offset + 0, meta['len_index_comp'])
+    # TODO: if verions > 2
+    block_decrypted = _mdx_decrypt(p)
+
+    def uncompressed_none(data):
+        return data
+
+    def uncompressed_lzo(data):
+        raise Exception('compress not support')
+
+    def uncompressed_zlib(data):
+        return zlib.decompress(data)
+
+    compress_type = part(block_decrypted, 0, 4)
+    d = dict([
+        (b'\x00\x00\x00\x00', uncompressed_none),
+        (b'\x01\x00\x00\x00', uncompressed_lzo),
+        (b'\x02\x00\x00\x00', uncompressed_zlib),
+    ])
+    compressed = part(block_decrypted, 8, meta['len_index_comp'] - 8)
+    uncompressed = d[compress_type](compressed)
+
+    b = part(block_decrypted, 4, 4)
+    checksum = uint_from_byte_be(b)
+
+    log(f'decompressed { uncompressed }')
+
+
+def _analyze_keyword_index(binary, offset, context):
+    t = part(binary, offset + 0, 4)
+    num_block0 = uint_from_byte_le(t)
+    log(f'num_block0: {num_block0}')
+
+    t = part(binary, offset + 4, 2)
+    len_keyword0 = uint_from_byte_le(t)
+    log(f'len_keyword0: {len_keyword0}')
+
+    c = part(binary, offset + 0, 100)
+    log(f'baoli {c}')
+
+    pass
 
 
 def main():
@@ -70,9 +136,7 @@ def main():
         binary = file.read()
 
         sh = analyze_section_header(binary, 0)
-        log(f'sh { sh }')
         sk = analyze_section_keyword(binary, sh['end'] )
-        log(f'sk { sk }')
 
 
 if __name__ == '__main__':
